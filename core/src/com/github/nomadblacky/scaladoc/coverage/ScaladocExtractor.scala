e6c709a9d1b685ad0883c4883148bbb5c183925c
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-12-25 01:47:00 +0900

    Add ScaladocExtractor.extractFromFile

--- core/src/com/github/nomadblacky/scaladoc/coverage/ScaladocExtractor.scala
+++ core/src/com/github/nomadblacky/scaladoc/coverage/ScaladocExtractor.scala
@@ -1,9 +1,24 @@
 package com.github.nomadblacky.scaladoc.coverage
 
-import scala.meta.Tree
+import java.nio.charset.Charset
+import java.nio.file.Path
+
 import scala.meta.contrib._
+import scala.meta.inputs.Input
+import scala.meta.parsers.Parsed
+import scala.meta.{Source, Tree}
 
 object ScaladocExtractor {
+  def extractFromFile(path: Path, charset: Charset): List[ScaladocComment] = {
+    val input = Input.File(path, charset)
+    input.parse[Source] match {
+      case Parsed.Success(tree) =>
+        extractFromTree(tree)
+      case e: Parsed.Error =>
+        throw e.details
+    }
+  }
+
   def extractFromTree(tree: Tree): List[ScaladocComment] = {
     val comments = AssociatedComments(tree)
 
--- core/test/src/com/github/nomadblacky/scaladoc/coverage/ScaladocExtractorTest.scala
+++ core/test/src/com/github/nomadblacky/scaladoc/coverage/ScaladocExtractorTest.scala
@@ -1,5 +1,8 @@
 package com.github.nomadblacky.scaladoc.coverage
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.Paths
+
 import utest._
 
 import scala.io.{Source => Resource}
@@ -7,32 +10,41 @@ import scala.meta.Source
 import scala.meta.inputs.Input
 
 object ScaladocExtractorTest extends TestSuite {
+  val expected = List(
+    ScaladocComment(
+      """/**
+        |  * The sample class.
+        |  */
+      """.stripMargin.trim
+    ),
+    ScaladocComment(
+      """/** This value is "foo" */"""
+    ),
+    ScaladocComment(
+      """/**
+        |  * Execute anything.
+        |  *
+        |  * @param n Double value
+        |  */
+      """.stripMargin.trim
+    )
+  )
+
   override def tests: Tests = Tests {
-    "extractFromTree" - {
+    "extractFromTree from Sample.scala" - {
       val url = getClass.getResource("Sample.scala")
       val code = Resource.fromURL(url).mkString
       val input = Input.VirtualFile(url.getPath, code)
       val tree = input.parse[Source].get
       val actual = ScaladocExtractor.extractFromTree(tree)
 
-      val expected = List(
-        ScaladocComment(
-          """/**
-            |  * The sample class.
-            |  */
-          """.stripMargin.trim
-        ),
-        ScaladocComment(
-          """/** This value is "foo" */"""
-        ),
-        ScaladocComment(
-          """/**
-            |  * Execute anything.
-            |  *
-            |  * @param n Double value
-            |  */
-          """.stripMargin.trim
-        )
+      assert(expected.head == actual.head)
+    }
+
+    "extractFromFile from Sample.scala" - {
+      val url = getClass.getResource("Sample.scala")
+      val actual = ScaladocExtractor.extractFromFile(
+        Paths.get(url.getPath), StandardCharsets.UTF_8
       )
 
       assert(expected.head == actual.head)
