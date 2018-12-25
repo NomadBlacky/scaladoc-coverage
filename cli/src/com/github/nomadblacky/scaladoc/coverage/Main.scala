793d713d59a9fbb6305ce42e9238ecd1bf0441f5
NomadBlacky <hourou.hamurinzu@gmail.com>
2018-12-25 22:10:51 +0900

    Fix CLI

--- cli/src/com/github/nomadblacky/scaladoc/coverage/Main.scala
+++ cli/src/com/github/nomadblacky/scaladoc/coverage/Main.scala
@@ -7,11 +7,30 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val sourceFiles = args.map(Paths.get(_)).toList
-    for {
+    val documentables = for {
       file <- sourceFiles
-      scaladoc <- DocumentableExtractor.extractFromFile(file, StandardCharsets.UTF_8)
+      documentable <- DocumentableExtractor.extractFromFile(file, StandardCharsets.UTF_8)
+    } yield documentable
+
+    val documentablesMap = documentables.groupBy(_.pkg).mapValues(_.groupBy(_.part))
+
+    // FIXME!!!
+    for {
+      (maybePackage, maybePartToDocable) <- documentablesMap
+      _ = println(maybePackage.getOrElse("(root)"))
+      (maybePart, docables) <- maybePartToDocable
+      _ = maybePart match {
+        case None =>
+          val d = docables.head
+          println(s"  ${d.name.get}: ${d.docText.isDefined}")
+        case Some(_) => ()
+      }
+      docable <- docables if docable.part.isDefined
     } {
-      println(scaladoc)
+      println(s"    ${docable.name.getOrElse("-")}: ${docable.docText.isDefined}")
     }
+
+    val coverage = documentables.count(_.docText.isDefined).toDouble / documentables.size * 100f
+    println(s"Coverage: $coverage%")
   }
 }
